ed98ce9bea2eed745f3f954a845acc3a16c88ee8
Erdemozer01 <ozer246@gmail.com>
2025-06-07 02:11:29 +0300

    düzeltme

--- dash_framework/urls.py
+++ dash_framework/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-app_name = 'dashboard_app' # Namespace için
+app_name = 'dash_framework' # Namespace için
 
 urlpatterns = [
     path('', views.dashboard_display_view, name='realtime_dashboard'),

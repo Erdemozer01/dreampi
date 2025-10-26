f0a7ec947060192c3c0cec6ad7b0a13b350196a1
Erdemozer01 <ozer246@gmail.com>
2025-10-26 06:12:19 +0300

    güncellendi

--- autonomous_drive_pi5.py
+++ autonomous_drive_pi5.py
@@ -22,10 +22,6 @@ import django
 from gpiozero import DistanceSensor, OutputDevice, Device
 from gpiozero.pins.lgpio import LGPIOFactory
 
-# Django'yu başlat
-sys.path.append('/home/pi')
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dream_pi.settings')
-django.setup()
 
 from scanner.models import Scan, ScanPoint
 
